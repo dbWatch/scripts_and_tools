c6515f957f07629f07046e74287ebe692776e1ba
Per Christopher <per.christopher@dbwatch.com>
2022-10-24 13:58:49 +0200

    Minor bugfix

--- Dynatrace_integration/Push_to_dyna.cmd
+++ Dynatrace_integration/Push_to_dyna.cmd
@@ -79,7 +79,7 @@ for /F "tokens=1-5 delims=;" %%a in (alarmlist.log) do (
 set /a var1-=1
 for /L %%G in (0,1,%var1%) do (
 REM Remove echo to actually run curl
-echo %CURL_EXEC% %CURL_OPTS% --data-raw '!array[%%G][1]: =.!,dt.entity.host=!array[%%G][0]!,!array[%%G][1]!="!array[%%G][4]:~0,100!" !array[%%G][3]!'
+echo %CURL_EXEC% %CURL_OPTS% --data-raw '!array[%%G][1]: =.!,dt.entity.host=!array[%%G][0]!,!array[%%G][1]: =.!="!array[%%G][4]:~0,100!" !array[%%G][3]!'
 )
 
 goto :clean_exit
