1ad0478498f84c8838d4ff38ba77681aef38da8d
Per Christopher <per.christopher@dbwatch.com>
2022-10-26 09:46:52 +0200

    Now strips \' from string

--- Dynatrace_integration/Push_to_dyna.cmd
+++ Dynatrace_integration/Push_to_dyna.cmd
@@ -73,9 +73,9 @@ for /F "tokens=1-5 delims=;" %%a in (alarmlist.log) do (
     set /a var2+=1
 	set array[!var1!][!var2!]=%%d
     set /a var2+=1
-	set array[!var1!][!var2!]=%%e
-REM	set tempval=!array[%var1%][%var2%]:e=blarg!
-REM	set array[!var1!][!var2!]=%tempval%
+ 	set array[!var1!][!var2!]=%%e
+	set tempval=!array[%var1%][%var2%]:'=!
+	set array[!var1!][!var2!]=!tempval!
 	set /a var1+=1
 )
 set /a var1-=1
