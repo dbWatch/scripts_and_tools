d973a35b60ece23610304f228e090575206dd2da
Per Christopher Undheim <per.christopher@dbwatch.com>
2022-12-22 12:11:44 +0100

    Now outputs in jobname in lower case

--- Dynatrace_integration/Push_to_dyna.cmd
+++ Dynatrace_integration/Push_to_dyna.cmd
@@ -36,7 +36,7 @@ call :log "Setting variables"
 SET me=%~n0
 SET parent=%~dp0
 set DBW_SERVER=server1
-set DBW_CLI_HOME=C:\Program Files\dbWatch\12.8.10\dbw\bin
+set DBW_CLI_HOME=C:\Program Files\dbWatch\12.8.11\dbw\bin
 set DBW_CLI_EXEC=dbw.exe
 set OLDPATH=%PATH%
 set CURL_EXEC=curl
@@ -45,7 +45,7 @@ set PATH=%DBW_CLI_HOME%;%PATH%
 set PATH=%DBW_CLI_HOME%;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\
 
 
-@REM %DBW_CLI_EXEC% ping -server %DBW_SERVER% > check_con.log
+%DBW_CLI_EXEC% ping -server %DBW_SERVER% > check_con.log 2>output.log
 FOR /F "delims=" %%x in ('findstr "OK" check_con.log ') DO (@set PING_ERR=NONE)
 IF "%PING_ERR%"=="" (
 	call :log "Connection to server failed"
@@ -58,7 +58,7 @@ IF "%PING_ERR%"=="NONE" (
 
 
 
-%DBW_CLI_EXEC% dbwql -server %DBW_SERVER% -query "instance->i/$i/name{}/$i/task[statusnumber > 0]->t/$t/name{}/$t/status{}/$t/statusnumber{}/$t/details{}" -format dynatrace > alarmlist.log
+%DBW_CLI_EXEC% dbwql -server %DBW_SERVER% -query "instance->i/$i/name{}/$i/task[statusnumber > 0]->t/$t/name{}/$t/status{}/$t/statusnumber{}/$t/details{}" -format dynatrace > alarmlist.log 2>output.log
 setlocal enabledelayedexpansion
 REM @echo off
 set var1=0
@@ -84,15 +84,18 @@ set tempval=!array[%%G][4]:~0,100!
 REM Remove echo to actually run curl
 
 set jobval_tmp=!array[%%G][1]: =.!
-echo Jobval_tmp: !jobval_tmp!
-echo > !jobval_tmp!
-dir /b/l %jobval_tmp% > lower.tmp
-set jobval=<lower.tmp
-del !jobval_tmp!
-@REM del lower.tmp
-echo Jobval: !jobval!
+@REM echo Jobval_tmp: !jobval_tmp!
+IF "!jobval_tmp!X"=="X" ( exit 0)
 
-echo %CURL_EXEC% %CURL_OPTS% --data-raw '!array[%%G][1]: =.!,dt.entity.host=!array[%%G][0]!,!array[%%G][1]: =.!="!tempval:'=!" !array[%%G][3]!'
+echo > !jobval_tmp!
+dir /b/l !jobval_tmp! > lower.tmp
+set /p jobval=<lower.tmp
+del !jobval_tmp!
+del lower.tmp
+@REM echo Jobval: !jobval!
+
+@REM OLD echo %CURL_EXEC% %CURL_OPTS% --data-raw '!array[%%G][1]: =.!,dt.entity.host=!array[%%G][0]!,!array[%%G][1]: =.!="!tempval:'=!" !array[%%G][3]!'
+echo %CURL_EXEC% %CURL_OPTS% --data-raw '!jobval!,dt.entity.host=!array[%%G][0]!,!jobval!="!tempval:'=!" !array[%%G][3]!'
 
 )
 
@@ -121,6 +124,7 @@ exit /b
 	call :log "Remove logfiles"
 	call :delfile alarmlist.log
 	call :delfile check_con.log
+	call :delfile output.log
 	rd config
 	exit /b
 
